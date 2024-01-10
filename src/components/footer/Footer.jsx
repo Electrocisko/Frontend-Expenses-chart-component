e10b9bff463540e5b466c1386a0d257880175f2e
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-10 16:39:59 -0300

    base app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.scss'
+
+function Footer() {
+  return (
+    <div className={styles.footer}>
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+    Coded by <a href="#">Francisco J Zuidwijk</a>.
+  </div>
+  )
+}
+
+export default Footer
