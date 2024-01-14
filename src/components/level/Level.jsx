da897b5f6f3ec8d8bff660b02769ece692101151
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-14 12:10:46 -0300

    working on levesl

--- src/components/level/Level.jsx
+++ src/components/level/Level.jsx
@@ -1,22 +1,31 @@
 /* eslint-disable react/prop-types */
-import styles from './level.module.scss'
+import { useState } from "react";
+import styles from "./level.module.scss";
 
-function Level({day,amount}) {
- 
-  let altura = `${amount*2}px`;
-  
+function Level({ day, amount }) {
+  const [color, setColor] = useState("hsl(10, 79%, 65%)");
+  const [hover, setHover] = useState(false);
+
+  const handleClick = () => {
+    setColor("hsl(186, 34%, 60%)");
+    setHover(true)
+  };
+
   return (
     <div className={styles.level_container}>
-        <p className={styles.amount}>{amount}</p>
-        <div className={styles.bar}  
-         style={{
-          height: altura
+      {hover && <p className={styles.amount}>{amount}</p>}
+      <div
+        onClick={handleClick}
+        style={{
+          backgroundColor: color,
+          margin: "6px",
+          borderRadius: "5px",
+          height: amount * 2,
         }}
-        ></div>
-        <p className={styles.day}>{day}</p>
-   
+      ></div>
+      <p className={styles.day}>{day}</p>
     </div>
-  )
+  );
 }
 
-export default Level
+export default Level;
