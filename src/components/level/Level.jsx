20fbd9d312e20651b2276a4a11edfacacd6d9657
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-17 19:21:07 -0300

    working on styles in levels

--- src/components/level/Level.jsx
+++ src/components/level/Level.jsx
@@ -2,33 +2,16 @@
 import { useState } from "react";
 import styles from "./level.module.scss";
 
-let softred = "hsl(10, 79%, 65%)";
-let cyan = "hsl(186, 34%, 60%)";
-let cyansoft = "hsl(187, 49%, 80%)";
-let verypaleorange= "hsl(10, 100%, 76%)";
 
 function Level({ day, amount, max }) {
-  const [color, setColor] = useState(softred);
-  const [colorCyan, setColorCyan] = useState(cyan);
+
   const [hover, setHover] = useState(false);
 
   const mouseEnter = () => {
-    setColor(verypaleorange);
     setHover(true);
-  };
-
-  const mouseEnterCyan = () => {
-    setColorCyan(cyansoft);
-    setHover(true);
-  };
-
-  const mouseLeaveCyan = () => {
-    setColorCyan(cyan);
-    setHover(false);
   };
 
   const mouseLeave = () => {
-    setColor(softred);
     setHover(false);
   };
 
@@ -37,27 +20,17 @@ function Level({ day, amount, max }) {
       {hover && <p className={styles.amount}>${amount}</p>}
       {max == amount ? (
         <div
-          onMouseEnter={mouseEnterCyan}
-          onMouseLeave={mouseLeaveCyan}
-          style={{
-            backgroundColor: colorCyan,
-            margin: "6px",
-            borderRadius: "5px",
-            height: amount * 2,
-            cursor: "pointer",
-          }}
+        // className={[styles.bar , styles.bar1] }
+        className={`${styles.bar} ${styles.bar2}`}
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
         ></div>
       ) : (
         <div
+        className={styles.bar}
           onMouseEnter={mouseEnter}
           onMouseLeave={mouseLeave}
-          style={{
-            backgroundColor: color,
-            margin: "6px",
-            borderRadius: "5px",
-            height: amount * 2,
-            cursor: "pointer",
-          }}
+
         ></div>
       )}
 
